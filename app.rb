bf26ef174c29fabf15e528534eefa7cfd99c6642
Kristen Kulha and Kelsey Langlois <kristen@>
2017-09-28 09:24:06 -0700

    add initial views

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+enable :sessions
+
+before do
+  @user = session[:id]
+end
+
+get('/') do
+  erb(:index)
+end

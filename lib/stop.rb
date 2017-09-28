0a5dd7167553fe3f66704a54bfc7e779e325576a
Kristen Kulha and Kelsey Langlois <kelsey@>
2017-09-28 13:20:47 -0700

    do the time thing finally.

--- lib/stop.rb
+++ lib/stop.rb
@@ -4,4 +4,8 @@ class Stop < ActiveRecord::Base
   belongs_to :city
   belongs_to :train
   validates :city, :train, :departure, presence: true
+
+  def departure_pst
+    self.departure.utc.localtime('-07:00')
+  end
 end
